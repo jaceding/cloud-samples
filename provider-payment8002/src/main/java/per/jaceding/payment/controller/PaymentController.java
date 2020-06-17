ef8d6b646292a966cae721960171987e9eb1560d
jaceding <931977674@qq.com>
2020-06-17 16:26:33 +0800

    feat:Eureka服务发现Discovery

--- provider-payment8002/src/main/java/per/jaceding/payment/controller/PaymentController.java
+++ provider-payment8002/src/main/java/per/jaceding/payment/controller/PaymentController.java
@@ -2,10 +2,17 @@ package per.jaceding.payment.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import per.jaceding.payment.entity.Payment;
 import per.jaceding.payment.service.PaymentService;
 import per.jaceding.payment.vo.Result;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 支付 控制层
@@ -20,6 +27,12 @@ public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     /**
      * 创建支付信息
@@ -50,6 +63,25 @@ public class PaymentController {
                 return Result.fail("查询失败，id:" + id);
             }
             return Result.success(payment);
+        } catch (Exception e) {
+            log.error("查询失败", e);
+        }
+        return Result.fail("查询失败");
+    }
+
+    /**
+     * 获取服务信息
+     */
+    @GetMapping("/discovery")
+    public Result getDiscovery() {
+        try {
+            Map<String, Object> dataMap = new HashMap<>(4, 1);
+            List<String> services = discoveryClient.getServices();
+            dataMap.put("services", services);
+            List<ServiceInstance> instances = discoveryClient.getInstances(applicationName);
+            dataMap.put("instances", instances);
+            dataMap.put("discoveryClient", discoveryClient);
+            return Result.success(dataMap);
         } catch (Exception e) {
             log.error("查询失败", e);
         }
