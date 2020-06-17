72775027d34a979671da1922128e1a9ee34b4352
jaceding <931977674@qq.com>
2020-06-17 12:56:21 +0800

    fix: 增加订单模块服务消费者

---
+++ consumer-order80/src/main/java/per/jaceding/order/config/RestConfiguration.java
@@ -0,0 +1,23 @@
+package per.jaceding.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestTemplate 配置
+ *
+ * @author jaceding
+ * @date 2020/6/17
+ */
+@Configuration
+public class RestConfiguration {
+
+    /**
+     * 注入 RestTemplate
+     */
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ consumer-order80/src/main/java/per/jaceding/order/controller/OrderController.java
@@ -0,0 +1,51 @@
+package per.jaceding.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import per.jaceding.payment.vo.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * 订单 控制层
+ *
+ * @author jaceding
+ * @date 2020/6/17
+ */
+@Slf4j
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    /**
+     * 支付模块地址
+     */
+    public static final String PAYMENT_URL = "http://127.0.0.1:8001/payment/";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping
+    public Result create() {
+        try {
+            String serial = UUID.randomUUID().toString();
+            log.info("serial#" + serial);
+            Map<String, String> body = new HashMap<>(2, 1f);
+            body.put("serial", UUID.randomUUID().toString());
+            Result result = restTemplate.postForObject(PAYMENT_URL, body, Result.class);
+            if (result != null && result.getCode() == 200) {
+                return Result.success("创建成功", body);
+            }
+            log.info("创建成功");
+        } catch (Exception e) {
+            log.error("创建失败", e);
+        }
+        return Result.fail("创建失败");
+    }
+}
