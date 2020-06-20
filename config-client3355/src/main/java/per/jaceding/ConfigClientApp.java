8e276956c58089d4119b214ff8e8a68a255a3693
jaceding <931977674@qq.com>
2020-06-20 11:01:14 +0800

    fix:添加Eureka的注解

--- config-client3355/src/main/java/per/jaceding/ConfigClientApp.java
+++ config-client3355/src/main/java/per/jaceding/ConfigClientApp.java
@@ -2,6 +2,7 @@ package per.jaceding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 主类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author jaceding
  * @date 2020/6/19
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApp {
 
